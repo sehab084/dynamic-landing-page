5aae8fcce666f70bca3ddb82af97944733b2eecf
Moheuddin Sehab <moheuddinsehab0@gmail.com>
2019-06-25 23:56:48 +0600

    finnaly added saving system

--- main.js
+++ main.js
@@ -34,32 +34,86 @@ showTime();
 function imageBackground() {
   let today = new Date(),
     sec = today.getSeconds();
-  console.log(sec);
+  // console.log(sec);
 
   if (sec < 10) {
     document.body.style.backgroundImage = "url('image1.jpg')";
     document.body.style.color = "black";
-    console.log("image1");
+    // console.log("image1");
   } else if (sec < 20) {
-    console.log("image2");
+    // console.log("image2");
     document.body.style.backgroundImage = "url('image2.jpg')";
   } else if (sec < 30) {
-    console.log("image3");
+    // console.log("image3");
     document.body.style.backgroundImage = "url('image3.jpg')";
     document.body.style.color = "white";
   } else if (sec < 40) {
-    console.log("image4");
+    // console.log("image4");
     document.body.style.color = "black";
     document.body.style.backgroundImage = "url('image4.jpg')";
   } else if (sec < 50) {
-    console.log("image5");
+    // console.log("image5");
     document.body.style.backgroundImage = "url('image5.jpg')";
     document.body.style.color = "white";
   } else {
-    console.log("image6");
+    // console.log("image6");
     document.body.style.backgroundImage = "url('image6.jpg')";
     document.body.style.color = "white";
   }
   setTimeout(imageBackground, 1000);
 }
 imageBackground();
+
+// get goal
+const goal = document.getElementById("goal");
+
+function getGoal() {
+  if (localStorage.getItem("goal") === null) {
+    goal.textContent = "[Enter Goal]";
+  } else {
+    goal.textContent = localStorage.getItem("goal");
+  }
+}
+//set goal
+function setGoal(e) {
+  if (e.type === "keypress") {
+    //make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("goal", e.target.innerText);
+      goal.blur();
+    }
+  } else {
+    localStorage.setItem("goal", e.target.innerText);
+  }
+  console.log(localStorage.getItem);
+}
+//goal.addEventListerner("keypress", setGoal);
+goal.addEventListener("blur", setGoal);
+goal.addEventListener("keypress", setGoal);
+//run function
+getGoal();
+
+//name
+const name = document.getElementById("name");
+
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Your Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+function setName(e) {
+  if (e.type == "keypress") {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+//run name fucntion and event lister
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+getName();
